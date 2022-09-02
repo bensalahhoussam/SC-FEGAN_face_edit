1c2bd6134871048ffa62309b3806f3043439e705
houssem_eddine_bensalah <112108580+bensalahhoussam@users.noreply.github.com>
2022-09-02 09:09:22 +0100

    Update build_SNGAN_discriminator.py

--- build_SNGAN_discriminator.py
+++ build_SNGAN_discriminator.py
@@ -4,12 +4,12 @@ from keras.models import Model
 from sn import SpectralNormalization
 
 def dis_conv(x):
-    x = SpectralNormalization(Conv2D(64, (3, 3),strides=2, activation=LeakyReLU(alpha=0.1)))(x)
-    x = SpectralNormalization(Conv2D(128, (3, 3), strides=2,activation=LeakyReLU(alpha=0.1) ))(x)
-    x = SpectralNormalization(Conv2D(256, (3, 3), strides=2,activation=LeakyReLU(alpha=0.1) ))(x)
-    x = SpectralNormalization(Conv2D(256, (3, 3), strides=2,activation=LeakyReLU(alpha=0.1)))(x)
-    x = SpectralNormalization(Conv2D(256, (3, 3),strides=2, activation=LeakyReLU(alpha=0.1)))(x)
-    x = SpectralNormalization(Conv2D(256, (3, 3), strides=2,activation=LeakyReLU(alpha=0.1)))(x)
+    x = SpectralNormalization(Conv2D(64*1, (3, 3),strides=2, activation=LeakyReLU(alpha=0.1)))(x)
+    x = SpectralNormalization(Conv2D(64*2, (3, 3), strides=2,activation=LeakyReLU(alpha=0.1) ))(x)
+    x = SpectralNormalization(Conv2D(64*4, (3, 3), strides=2,activation=LeakyReLU(alpha=0.1) ))(x)
+    x = SpectralNormalization(Conv2D(64*4, (3, 3), strides=2,activation=LeakyReLU(alpha=0.1)))(x)
+    x = SpectralNormalization(Conv2D(64*4, (3, 3),strides=2, activation=LeakyReLU(alpha=0.1)))(x)
+    x = SpectralNormalization(Conv2D(64*4, (3, 3), strides=2,activation=LeakyReLU(alpha=0.1)))(x)
     return x
 inputs = Input(shape=(512,512,5))
 output=dis_conv(inputs)
