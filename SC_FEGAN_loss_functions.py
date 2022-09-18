dad7a6fc4a0b2c61767015f89a99f8164b07c68b
houssem_eddine_bensalah <112108580+bensalahhoussam@users.noreply.github.com>
2022-09-18 11:52:46 +0100

    Update SC_FEGAN_loss_functions.py

--- SC_FEGAN_loss_functions.py
+++ SC_FEGAN_loss_functions.py
@@ -143,3 +143,20 @@ def discriminator_loss(dis_real, dis_fake):
     dis_loss = tf.reduce_mean(tf.maximum(1 - dis_real, 0)) + \
                tf.reduce_mean(tf.maximum(1 + dis_fake, 0))
     return dis_loss
+
+def gradient_penalty(model,ground_truth, complete_image, mask):
+    batch,w,h,c = ground_truth.get_shape().as_list()
+
+    alpha = tf.random.normal([batch, 1, 1, 1], 0.0, 1.0)
+    diff = complete_image - ground_truth
+    interpolated = complete_image + alpha * diff
+
+    with tf.GradientTape() as gp_tape:
+        gp_tape.watch(interpolated)
+        pred = model(interpolated, training=True)
+    grads = gp_tape.gradient(pred, [interpolated])[0]
+    norm = tf.sqrt(tf.reduce_sum(tf.square(grads), axis=[1, 2, 3]))
+    gp = tf.reduce_mean((norm * mask - 1.0) ** 2)
+    return gp
+
+
