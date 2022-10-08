cdaf9d5bbd06c6e2d9fd9ad6e6bbe7519d70af75
houssem_eddine_bensalah <112108580+bensalahhoussam@users.noreply.github.com>
2022-10-08 14:20:49 +0100

    Update build_SNGAN_discriminator.py

--- build_SNGAN_discriminator.py
+++ build_SNGAN_discriminator.py
@@ -26,15 +26,37 @@ class discriminator_block(Model):
         return x
 
 
-class Discriminator(Model):
+class discriminator_block_gated_conv(Model):
     def __init__(self, ):
+        super(discriminator_block, self).__init__()
+        self.x_1=SpectralNormalization(Gated_Convolutional(filters=64 * 1, lrn=False, dilation_rate=1, strides=1, num_block=1))
+        self.x_2=SpectralNormalization(Gated_Convolutional(filters=64 * 2, lrn=False, dilation_rate=1, strides=2, num_block=2))
+        self.x_3=SpectralNormalization(Gated_Convolutional(filters=64 * 4, lrn=False, dilation_rate=1, strides=2, num_block=3))
+        self.x_4=SpectralNormalization(Gated_Convolutional(filters=64 * 4, lrn=False, dilation_rate=1, strides=2, num_block=4))
+        self.x_5=SpectralNormalization(Gated_Convolutional(filters=64 * 4, lrn=False, dilation_rate=1, strides=2, num_block=5))
+        self.x_6=SpectralNormalization(Gated_Convolutional(filters=64 * 4, lrn=False, dilation_rate=1, strides=2, num_block=6))
+
+    def call(self, inputs, training=True, **kwargs):
+        x=self.x_1(inputs)
+        x=self.x_2(x)
+        x=self.x_3(x)
+        x=self.x_4(x)
+        x=self.x_5(x)
+        x=self.x_6(x)
+        return x
+
+class Discriminator(Model):
+    def __init__(self, gated=False):
         super(Discriminator, self).__init__()
+        self.switch=gated
         self.dis_block=discriminator_block()
+        self.dis_gated = discriminator_block_gated_conv()
+        
     def call(self, inputs, training=True,**kwargs):
-        output=self.dis_block(inputs)
+        if self.switch == False:
+            output=self.dis_block(inputs)
+        else: 
+            output=self.dis_gated(inputs)
+            
         model=Model(inputs=inputs,outputs=output)
         return model
-
-model=Discriminator()
-model=model.call(inputs)
-print(model.summary())
